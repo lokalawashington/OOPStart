afe21e9516280d8191270367f5aec463db4b71c6
peter ngotho <sammykamau772@mail.com>
2021-06-14 05:14:26 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import another_module
+#import turtle
+"""check docs.python.org/library/turtle.html for more methods."""
+import turtle
 
-print(another_module.another_variable)
+import another_module
+from turtle import Turtle, Screen
+
+#print(another_module.another_variable)
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(200)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+#method exitonclick()
+my_screen.exitonclick()
+
+
